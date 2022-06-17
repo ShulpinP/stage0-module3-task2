83f2c61c60f10602c63f29b77bceff64795c7b0b
ShulpinP <pavelshulpin6@gmail.com>
2022-06-17 20:47:39 +0300

    names

---
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -0,0 +1,17 @@
+package lang.print.gaps.task2;
+
+public class AdvancedNamingConvention {
+    private static final int adultAge = 18;
+
+    private int Age;
+    private int phoneNumber;
+
+    void callByName() {
+        //some code
+    }
+
+    void callByNumber(int number) {
+        //some code
+    }
+}
+
